cf76698ea8b8643da85e92c1aedcc0ddfaddff24
Josh Brooks <defaultjosh@hotmail.com>
2020-07-04 22:45:20 -0400

    Added a div. Also, Searchbox and its props

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,9 @@ import "../styles/Nav.css";
 function Nav({ handleSearchChange }) {
   return (
     <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="navbar-collapse">
+        <SearchBox handleSearchChange={handleSearchChange} />
+      </div>
      {/* We render the SearchBox here and pass in necessary props */}
     </nav>
   );
